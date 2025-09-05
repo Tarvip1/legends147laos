aea0e463839237b319915c53be05576a45605f55
Tarvip1 <saruregency@gmail.com>
2025-09-05 12:07:16 +0700

    Create getHost.js

---
+++ netlify/functions/getHost.js
@@ -0,0 +1,10 @@
+exports.handler = async function(event, context) {
+  const urls = [
+    "https://legend147-bamboora.store",
+    "https://legend147-Lilyvia.store"
+  ];
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ urls })
+  };
+};
